2850eb2cb14986eb2caaedc4d2b658d4247844d9
Dominik Soós <soos.domi@gmail.com>
2025-03-13 11:08:16 -0400

    parallelpsokernel results

--- related/psogpu/rosenbrock.jl
+++ related/psogpu/rosenbrock.jl
@@ -5,7 +5,9 @@ using SciMLBase
 using PSOGPU, StaticArrays, KernelAbstractions, Optimization
 using CUDA
 
-using OptimizationBase
+backend = CUDABackend()
+
+#using OptimizationBase
 
 using BenchmarkTools
 
@@ -23,42 +25,48 @@ end
 #    sum(p[1] + x[i]^2 - p[1] * cos(p[2]*Float32(π) * x[i]) for i in 1:length(x)-1))
 #end
 N = 10
-x0 = @SArray zeros(Float32, 10)
-p = @SArray Float32[1.0, 100.0]
+
 x0 = @SArray zeros(Float32, N)
 p = @SArray Float32[1.0, 100.0]
 lb = @SArray fill(Float32(-1.0), N)
 ub = @SArray fill(Float32(10.0), N)
 optf = OptimizationFunction(rosenbrock, Optimization.AutoForwardDiff())
-prob = OptimizationProblem(optf, x0, p; lb = lb, ub = ub)
+prob = OptimizationProblem(optf, x0, p)#; lb = lb, ub = ub)
 
 #prob = OptimizationProblem(rosenbrock, x0, p; lb = lb, ub = ub)
 
-n = 512
+n = 8192
 println("number of particles: ", n)
 #n = 1024 * 1024
  
 #optf = OptimizationFunction(rosenbrock, Optimization.AutoForwardDiff())
 #prob = OptimizationProblem(rosenbrock, x0, p; lb=lb, ub=ub)
-l0 = rosenbrock(x0, p)
+#l0 = rosenbrock(x0, p)
 
-sol = solve(prob, PSOGPU.HybridPSO(; backend = CUDABackend() ,pso = PSOGPU.ParallelPSOKernel(n, backend = CUDA.CUDABackend())), maxiters = 500)
- 
+#sol = solve(prob, PSOGPU.HybridPSO(; backend = CUDABackend() ,pso = PSOGPU.ParallelPSOKernel(n, backend = CUDA.CUDABackend())), maxiters = 500)
+#sol = solve(prob,
+#      ParallelSyncPSOKernel(N; backend = CUDABackend()),
+#      maxiters = 500) 
 #sol = solve(prob, PSOGPU.HybridPSO(; backend = CUDABackend() ,pso = PSOGPU.ParallelPSOKernel(n;global_update = false,backend = CUDABackend()),local_opt = PSOGPU.LBFGS()), maxiters = 500,local_maxiters = 30)
-@show sol.objective
-@show sol.stats.time
-
+#@show sol.objective
+#@show sol.stats.time
 
 #sol = solve(prob, HybridPSO( backend = CUDA.CUDABackend(), local_opt = PSOGPU.BFGS())) # local_maxiters = 30)
 #@show sol.objective
 
 benchmark_result = @benchmark solve(prob, ParallelPSOKernel(n, backend = CUDA.CUDABackend()), maxiters = 500)
 time_taken = median(benchmark_result).time / 1e9
+#@time sol = Optimization.solve(prob,
+#    PSOGPU.ParallelPSOKernel(100; backend),
+#    maxiters = 100)
+#@show sol.objective
 
 #sol = solve(prob, ParallelSyncPSOKernel(n, backend = CUDA.CUDABackend()), maxiters = 500)
 sol = solve(prob, ParallelPSOKernel(n, backend = CUDA.CUDABackend()), maxiters = 500)
+#time_taken = median(sol).time / 1e9
 #sol = solve(prob, ParallelSyncPSOKernel(1024*10243, backend = CUDA.CUDABackend()), maxiters = 500)
 # Print timing and optimization results
+
 println("Median execution time: ", time_taken, " seconds")
 println("Optimal parameters found: ", sol.minimizer)
 println("Minimum value of the objective function: ", sol.minimum)
