c5aadb5735d9d86b585b3f3777eef777176bc353
Dominik Soós <soos.domi@gmail.com>
2025-07-14 15:22:41 -0400

    metaprogramming to let user std::array<T,N> instead of explicit N/DIM

--- metaprogramming_t.cpp
+++ metaprogramming_t.cpp
@@ -8,18 +8,17 @@ square(double x)
   return x * 2.5;
 }
 
-static constexpr int d = 10;
-
 struct Rosen {
-  static constexpr int DIM = d;
+  //static constexpr int DIM = d;
 
-  template <class T>
-  __host__ __device__ T
-  operator()(const T* x, int DIM=d) const
+  template <class T, std::size_t N>
+  __host__ __device__
+  T operator()(std::array<T, N>const& x) const
+  //operator()(const T* x, int DIM=d) const
   {
     T sum = T(0);
 #pragma unroll
-    for (int i = 0; i < DIM-1; ++i) {
+    for (int i = 0; i < N-1; ++i) {
       T t1 = T(1) - x[i];
       T t2 = x[i + 1] - x[i] * x[i];
       sum += t1 * t1 + T(100) * t2 * t2;
@@ -47,7 +46,8 @@ main()
   }
   util::set_stack_size();
 
+  std::array<double,10> x0{};
   auto res = zeus::Zeus(
-    Rosen{}, -5.12, 5.12, host, 1024, 10000, 5, 100, "rosenbrock", 1e-8, 42, 0);
+    Rosen{},x0, -5.12, 5.12, host, 1024, 10000, 5, 100, "rosenbrock", 1e-8, 42, 0);
   std::cout << "global minimum for rosenbrock: " << res.fval << std::endl;
 }
