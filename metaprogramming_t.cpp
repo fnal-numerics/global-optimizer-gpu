742e2fc73b6cfa2230a810af4afa125d4ab306f5
Dominik Soós <soos.domi@gmail.com>
2025-07-18 10:46:47 -0400

    static assert in zeus + gaussian

--- metaprogramming_t.cpp
+++ metaprogramming_t.cpp
@@ -30,20 +30,27 @@ struct Rast {
   __host__ __device__
   constexpr T operator()(const std::array<T,N>& x) const {
     T sum = T(10.0 * N);
+    using namespace std;
   #pragma unroll
     for (std::size_t i = 0; i < N; ++i) {
-      if constexpr (std::is_same_v<T,double>) {
-        sum += x[i]*x[i]
-             - T(10.0) * T(std::cos(2.0 * M_PI * x[i]));
-      } else {
-        sum += x[i]*x[i]
-             - T(10.0) * dual::cos(x[i] * T(2.0 * M_PI));
-      }
+      sum += x[i]*x[i]
+             - T(10.0) * cos(2.0 * M_PI * x[i]);
     }
     return sum;
   }
 };
+/*
+struct Gaussian {
+  private:
+    std::array<T,N> covar;// argument to the constructor
 
+  template<class T, std::size_t N>
+  __host__ __device__
+  constexpr T operator()(const std::array<T,N>& x, covar) const {
+    
+  }
+};
+*/
 
 int
 main()
@@ -65,13 +72,16 @@ main()
 
   std::array<double,10> x10{};
   auto res10 = zeus::Zeus(Rosen{},x10, -5.12, 5.12, host, N, 10000, 5, 100, "rosenbrock", 1e-8, 42, 0);
-  std::array<double, 128> x3{};
+  std::array<double, 3> x3{};
   auto res3 = zeus::Zeus(Rosen{},x3, -5.12, 5.12, host, 1, 100, 0, 1, "rosenbrock", 1e-8, 42, 0);
   std::cout << "global minimum for 10d rosenbrock: " << res10.fval << std::endl;
-  std::cout << "global minimum for 128d rosenbrock: " << res3.fval << std::endl;
+  std::cout << "global minimum for 3d rosenbrock: " << res3.fval << std::endl;
 
   std::array<double, 5> x5{};
   auto res5 = zeus::Zeus(Rast{},x5, -5.12, 5.12, host, N, 10000, 25, 100, "rastrigin", 1e-8, 42, 0);
   std::cout << "global minimum for 5d rastrigin: " << res5.fval << std::endl;
  
+  // positive symmetric matrix
+  // matrix of random numbers -> transpose to itself, divide by 2.
+  //   
 }
