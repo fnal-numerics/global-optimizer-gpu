9c5652dcb1de930761894d4cc92bbf34305bc17f
Dominik Soós <soos.domi@gmail.com>
2025-07-22 12:47:13 -0400

    gaussian: synchost2gpu +  removed comments

--- gaussian.hpp
+++ gaussian.hpp
@@ -5,34 +5,6 @@
 
 #include "matrix.cuh"
 
-// template for square matrix
-//   type of the thing
-//   NxN
-// allocate memory
-
-// function to access the elements
-// for i,j
-
-// matrix class template
-// class constructor gets told the size of the array
-
-// copy the buffer
-// class manage its own resources
-
-
-  // copy constructor, gotta call cudamalloc
- // copy assignment operator, not a constructor --> 
-
- // move assignment
-// destructor
-// assignment operator=();
-// copy and swap idiom
-
-  // constructor to allocate matrxi on host + device
-  // destructor to get rid of the covariance matrix 
-
-
-
 template <std::size_t N>
 class Gaussian {
   Matrix<double> C;
@@ -45,6 +17,9 @@ public:
     for (std::size_t i = 0; i < N; ++i)
       for (std::size_t j = 0; j < N; ++j)
         C(i,j) = C_host[i][j];
+    
+    // push buffer to the GPU
+    C.syncHostToDevice();
   }
 
   // device & host call-operator
