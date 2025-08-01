ec157cbd94740cd4d916419759d993fbdb5054b5
Dominik Soós <soos.domi@gmail.com>
2025-08-01 13:49:05 -0400

    set up to use free function, removed init array as we're deducing DIM from the function

--- benchmarks/examples/metaprogramming_t.cpp
+++ benchmarks/examples/metaprogramming_t.cpp
@@ -36,13 +36,9 @@ struct Rosen {
 };
 
 // templated Rastrigin on T (double or Dual) and N at compile time
-struct Rast {
-  Rast(Rast const&) = delete;
-  Rast& operator=(Rast const&) = delete;
-
-  template<class T, std::size_t N>
+  template<typename T, std::size_t N>
   __host__ __device__
-  constexpr T operator()(const std::array<T,N>& x) const {
+  constexpr T rast(const std::array<T,N>& x) {
     T sum = T(10.0 * N);
     using namespace std;
   #pragma unroll
@@ -52,10 +48,9 @@ struct Rast {
     }
     return sum;
   }
-};
 
-template<std::size_t D>
-double square2(std::array<double,D> const& x) {
+template<typename T, std::size_t D>
+T square2(std::array<T,D> const& x) {
     double sum = 0.0;
     for (std::size_t i = 0; i < D; ++i) {
         sum += x[i] * x[i];
@@ -84,22 +79,19 @@ main(int argc, char* argv[])
   util::set_stack_size();
   
 #if(0)
-  std::array<double,10> x10{};
-  auto res10 = zeus::Zeus(Rosen{},x10, -5.12, 5.12, host, N, 10000, 5, 100, "rosenbrock", 1e-8, 42, 0);
-  std::array<double, 3> x3{};
-  auto res3 = zeus::Zeus(Rosen{},x3, -5.12, 5.12, host, 1, 100, 0, 1, "rosenbrock", 1e-8, 42, 0);
+  auto res10 = zeus::Zeus(Rosen{}, -5.12, 5.12, host, N, 10000, 5, 100, "rosenbrock", 1e-8, 42, 0);
+  auto res3 = zeus::Zeus(Rosen{}, -5.12, 5.12, host, 1, 100, 0, 1, "rosenbrock", 1e-8, 42, 0);
   std::cout << "global minimum for 10d rosenbrock: " << res10.fval << std::endl;
   std::cout << "global minimum for 3d rosenbrock: " << res3.fval << std::endl;
 
-  std::array<double, 5> x5{};
-  auto res5 = zeus::Zeus(Rast{},x5, -5.12, 5.12, host, N, 10000, 10, 100, "rastrigin", 1e-8, 42, 0);
+  auto res5 = zeus::Zeus(Rast{}, -5.12, 5.12, host, N, 10000, 10, 100, "rastrigin", 1e-8, 42, 0);
   std::cout << "global minimum for 5d rastrigin: " << res5.fval << std::endl;
 #endif
 #if(1)
   // positive symmetric matrix
   // matrix of random numbers -> transpose to itself, divide by 2.
   //   
-  //auto result2 = zeus::Zeus(square2<2>,-5.0, 5.0,100,1000,100,10,"square",1e-6,42,0);
+  //auto result2 = zeus::Zeus(rast,-5.0, 5.0,100,1000,100,10,"square",1e-6,42,0);
 
   constexpr std::size_t D = 4;
   using T = double;
@@ -114,11 +106,6 @@ main(int argc, char* argv[])
 
   Gaussian<D> g{C}; 
 
-  /*std::array<T, D> x150{};
-  x150.fill(T(3.7));
-  T fx = g(x150);
-  std::cout << "f(x) = " << fx << std::endl;
-  */
   std::cout << "running " << D << "d Gaussian minimization" << std::endl; 
   using namespace std::literals;
   auto res150 = zeus::Zeus(g, -5.00, 5.00, N, 10000, 10, 100, "gaussian"s, 1e-8, 42, run); 
